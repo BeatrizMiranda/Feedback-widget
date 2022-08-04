fab73c3d1b39773644ef02c15e849412762872a8
Beatriz Miranda <beatriz.miranda@thoughtworks.com>
2022-08-04 15:43:59 -0300

    fix: try again to fix value of macContentLength

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 import cors from 'cors'
 import { routes } from './routes'
 
@@ -7,6 +7,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(json({
+  limit: '20mb'
+}));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('server running')
